1c72e64c7a94d9e65d708e3cd7ee076587f35269
Jiwon Yeom <yjw113080@gmail.com>
2021-03-15 16:26:54 +0900

    Update init.sh
    source ~/.bash_profile added

--- init.sh
+++ init.sh
@@ -1,3 +1,20 @@
+# env configuration
+echo '============================================'
+echo 'Configuring the environment'
+echo '============================================'
+rm -vf ${HOME}/.aws/credentials
+
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
+
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
+echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
+aws configure set default.region ${AWS_REGION}
+aws configure get default.region
+source ~/.bash_profile
+
 # kubectl
 sh ~/environment/init/resize.sh 60
 echo '============================================'
@@ -21,21 +38,7 @@ echo 'yq() {
 }' | tee -a ~/.bashrc && source ~/.bashrc
 curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-# env configuration
-echo '============================================'
-echo 'Configuring the environment'
-echo '============================================'
-rm -vf ${HOME}/.aws/credentials
 
-export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
-export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
-
-echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
-echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
-echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
-aws configure set default.region ${AWS_REGION}
-aws configure get default.region
 
 # eksctl prereqs
 echo '============================================'
@@ -46,6 +49,7 @@ aws ec2 import-key-pair --key-name "eksworkshop" --public-key-material file://~/
 aws kms create-alias --alias-name alias/eksworkshop --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
 export MASTER_ARN=$(aws kms describe-key --key-id alias/eksworkshop --query KeyMetadata.Arn --output text)
 echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
+source ~/.bash_profile
 
 # eksctl
 echo '============================================'
